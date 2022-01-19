be67e97bb3e85d17b35d5f840004f65d69548449
Evgeni <my@example.com>
2022-01-19 11:08:30 +0300

    added added new imports

--- create_exe.sh
+++ create_exe.sh
@@ -1,2 +1,13 @@
-pyinstaller --onefile --hidden-import=time --hidden-import=datetime  --hidden-import=smtplib --hidden-import=json --hidden-import=os --hidden-import=sys --hidden-import=email.mime --windowed result.py
- --hidden-import=
+pyinstaller --onefile \
+            --hidden-import=time \
+            --hidden-import=datetime \
+            --hidden-import=smtplib \
+            --hidden-import=json \
+            --hidden-import=os \
+            --hidden-import=sys \
+            --hidden-import=email.mime \
+            --hidden-import=email.encoders \
+            --hidden-import=zipfile \
+            --distpath ./result \
+            --windowed result.py
+# --hidden-import=
